610dd204a05bd73f2899bdb20f28359f26259193
jerbin1122 <jerbin1122@gmail.com>
2026-05-27 18:53:50 +0530

    update chat

--- server.js
+++ server.js
@@ -5,80 +5,120 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
+
 const server = http.createServer(app);
+
 const io = new Server(server);
 
 const FILE = path.join(__dirname, "messages.json");
 
 /* LOAD DATA */
-function loadData() {
-    try {
+function loadData(){
+
+    try{
+
         return JSON.parse(fs.readFileSync(FILE, "utf8"));
-    } catch (err) {
-        return { messages: [], users: {} };
+    }
+    catch(err){
+
+        return {
+            messages: [],
+            users: {}
+        };
     }
 }
 
 /* SAVE DATA */
-function saveData(data) {
+function saveData(data){
+
     fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
+/* DATA */
 let data = loadData();
+
 let messages = data.messages;
+
 let users = data.users;
 
-app.get("/", (req, res) => {
+/* FRONTEND */
+app.get("/", (req,res)=>{
+
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
+/* SOCKET */
+io.on("connection", (socket)=>{
+
     console.log("User connected");
 
-    // send chat history
+    /* SEND OLD MESSAGES */
     socket.emit("load messages", messages);
 
-    // send online users
-    io.emit("users online", Object.values(users));
+    /* RESTORE SAVED NAME */
+    socket.on("get saved name", (userId)=>{
 
-    socket.on("chat message", (dataMsg) => {
+        if(users[userId]){
 
-        if (!dataMsg || !dataMsg.name || !dataMsg.msg) return;
+            socket.emit("saved name", users[userId]);
+        }
+    });
 
-        // SAVE USER ON SERVER (NO LOCALSTORAGE)
-        users[socket.id] = dataMsg.name;
+    /* SEND MESSAGE */
+    socket.on("chat message", (dataMsg)=>{
+
+        if(!dataMsg) return;
+
+        if(!dataMsg.userId) return;
+
+        if(!dataMsg.name) return;
+
+        if(!dataMsg.msg) return;
+
+        /* SAVE USER NAME */
+        users[dataMsg.userId] = dataMsg.name;
 
         const msgData = {
-            name: dataMsg.name,
-            msg: dataMsg.msg,
-            time: Date.now()
+            name:dataMsg.name,
+            msg:dataMsg.msg,
+            time:Date.now()
         };
 
+        /* SAVE MESSAGE */
         messages.push(msgData);
 
-        saveData({ messages, users });
+        saveData({
+            messages:messages,
+            users:users
+        });
 
+        /* SEND TO EVERYONE */
         io.emit("chat message", msgData);
+
+        /* ONLINE USERS */
         io.emit("users online", Object.values(users));
     });
 
-    socket.on("clear messages", () => {
+    /* CLEAR CHAT */
+    socket.on("clear messages", ()=>{
+
         messages = [];
-        saveData({ messages, users });
+
+        saveData({
+            messages:messages,
+            users:users
+        });
+
         io.emit("load messages", []);
     });
 
-    socket.on("disconnect", () => {
-        delete users[socket.id];
-
-        saveData({ messages, users });
-
-        io.emit("users online", Object.values(users));
+    socket.on("disconnect", ()=>{
 
         console.log("User disconnected");
     });
 });
 
-server.listen(3000, () => {
+server.listen(3000, ()=>{
+
     console.log("Server running on http://localhost:3000");
 });
